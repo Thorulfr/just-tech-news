157a19ad2245d2ab95796980fee9362dcfe9d01f
Ben H <deepfreespace@gmail.com>
2021-12-06 18:26:01 -0700

    Public folder

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+// Imports
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
 
+// Declarations
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Express configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Turn on routes
 app.use(routes);
