4acb11ae20b695dcbe9ad7fa5ce14ecc08f9c7cf
Ben H <deepfreespace@gmail.com>
2021-12-07 22:38:36 -0700

    Finished homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,54 @@
 // Imports
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 // Router config
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}],
-        user: {
-            username: 'test_user',
-        },
-    });
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+                sequelize.literal(
+                    '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+                ),
+                'vote_count',
+            ],
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at',
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            // Serialize post array
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            // Pass a single post object into the homepage template
+            res.render('homepage', { posts });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
