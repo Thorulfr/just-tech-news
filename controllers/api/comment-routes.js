31cfda537c038b576c5d79ad13c6b4a2b30265f6
Ben H <deepfreespace@gmail.com>
2021-12-11 19:13:43 -0700

    Conditional login/out

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,46 +1,54 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// Get all comments
 router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
+// Create new comment
 router.post('/', (req, res) => {
-  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id
-  })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    // check the session
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from the session
+            user_id: req.session.user_id,
+        })
+            .then((dbCommentData) => res.json(dbCommentData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
+// Delete a comment
 router.delete('/:id', (req, res) => {
-  Comment.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCommentData => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: 'No comment found with this id!' });
-        return;
-      }
-      res.json(dbCommentData);
+    Comment.destroy({
+        where: {
+            id: req.params.id,
+        },
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then((dbCommentData) => {
+            if (!dbCommentData) {
+                res.status(404).json({
+                    message: 'No comment found with this id!',
+                });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { route } = require('.');
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// get all posts for homepage
+// Get all posts for homepage
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
@@ -41,8 +42,10 @@ router.get('/', (req, res) => {
     })
         .then((dbPostData) => {
             const posts = dbPostData.map((post) => post.get({ plain: true }));
-
-            res.render('homepage', { posts });
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn,
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -50,6 +53,7 @@ router.get('/', (req, res) => {
         });
 });
 
+// Login page (and reroute to home if already logged in)
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -57,6 +61,66 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+// Single post page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+                sequelize.literal(
+                    '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+                ),
+                'vote_count',
+            ],
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at',
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
